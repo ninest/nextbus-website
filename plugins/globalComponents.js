23f43d65ed4fc78076a25d43a554e4a25713437e
themindstorm <parth.kabra@gmail.com>
2020-04-23 00:29:38 +0800

    Changed @ to ~

--- plugins/globalComponents.js
+++ plugins/globalComponents.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import ImgComp from '@/components/article/ImgComp.vue';
-import Clr from '@/components/article/Clr.vue';
+import ImgComp from '~/components/article/imgComp.vue';
+import Clr from '~/components/article/Clr.vue';
 
 Vue.component('ImgComp', ImgComp);
 Vue.component('Clr', Clr);
